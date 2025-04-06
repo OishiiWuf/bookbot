9aeb33a012e3d71aa6a4f57de4579625597e72e4
OishiiWuf <oishiigithub@wolfie.tv>
2025-04-05 23:46:07 -0600

    Make bookbot dynamic and accept filepaths to books, additional warning for incorrect usage args

--- main.py
+++ main.py
@@ -2,16 +2,22 @@ from stats import get_num_words
 from stats import get_num_characters
 from stats import get_sorted_list
 
+import sys
+
 def get_book_text(filepath):
     with open(filepath) as book_file:
         return book_file.read()
 
 def main():
-    book_text = get_book_text("books/frankenstein.txt")
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+
+    book_text = get_book_text(sys.argv[1])
     num_words = get_num_words(book_text)
     sorted_list = get_sorted_list(get_num_characters(book_text))
     print("============ BOOKBOT ============")
-    print("Analyzing book found at books/frankenstein.txt...")
+    print(f"Analyzing book found at {sys.argv[1]}")
     print("----------- Word Count ----------")
     print(f"Found {num_words} total words")
     print("--------- Character Count -------")
